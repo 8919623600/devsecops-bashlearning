c2ec2db1ea24a44cadcffed6d2bace5ae2d0624e
Jyoti <ilu29jan@gmail.com>
2026-05-08 22:03:59 +0530

    code updated

--- roboshop-bash/frontend.sh
+++ roboshop-bash/frontend.sh
@@ -3,8 +3,10 @@
 echo "Configuration management for Frontend"
 
 echo "disabling nginx module"
-
-
+dnf module disable nginx -y
 
 echo "Enabling nginx"
 dnf module enable nginx:1.24 -y
+
+echo "Installing Nginx"
+dnf install nginx -y
