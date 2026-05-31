92739f65d80c2d35951e2c3954f1a3db3aeb7922
Jyoti <ilu29jan@gmail.com>
2026-05-31 17:31:03 +0530

    file updated

--- roboshop-bash/mongodb.sh
+++ roboshop-bash/mongodb.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 COMPONENT="mongodb"
 
-if [$ID -ne 0]; then
+if [ $ID -ne 0 ]; then
    echo -e "Script has to be excecuted by root user"
    echo -e "Example usage: sudo $0.sh "
    exit 1
