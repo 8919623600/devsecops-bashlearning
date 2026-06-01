550d8347ed4b3a847b8b7c64fb60c962eb6aa170
Jyoti <ilu29jan@gmail.com>
2026-06-02 01:41:11 +0530

    file updated

---
+++ roboshop-bash/redis.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+dnf module list
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y
+
+sed -ie 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -ie 's/127.0.0.1/0.0.0.0/' /etc/redis.conf/etc/redis/redis.conf
+
+sed -i 's/^protected-mode yes/protected-mode no/' /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl start redis
+systemctl status redis -l
